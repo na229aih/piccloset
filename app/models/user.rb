10e921e55047407d2e3efaf056f30ec273b64d3f
na229aih <naoya.aihara.1127@gmail.com>
2019-10-01 13:41:47 +0900

    commentモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
           
   has_many :items
+  has_many :comments
   mount_uploader :profile, ImageUploader
 end
