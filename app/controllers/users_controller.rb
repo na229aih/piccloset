d225fc2d79dc103a3247f98dc6b5521a29f7bb3f
na229aih <naoya.aihara.1127@gmail.com>
2019-10-02 21:15:39 +0900

    ユーザー画面ビュー調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
+    @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id]).order("id DESC")
   end
 
